147aa7db87141577fe30940990e168c4ef6d15f7
Naman Agarwal <f20180435@pilani.bits-pilani.ac.in>
2020-01-25 20:21:05 +0530

    many changes

--- app/src/main/java/com/example/mapbox/MainActivity.java
+++ app/src/main/java/com/example/mapbox/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Toast toast = Toast.makeText(getApplicationContext(),
+//                "This is a message displayed in a Toast",
+//                Toast.LENGTH_SHORT);
+//
+//        toast.show();
 
         btn = findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/mapbox/MapBox.java
+++ app/src/main/java/com/example/mapbox/MapBox.java
@@ -24,7 +24,10 @@ import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -75,7 +78,7 @@ public class MapBox extends AppCompatActivity implements
     private LocationComponent locationComponent;
 
     // variables needed to initialize navigation
-    private Button navBtn,currentLocBtn;
+    private Button navBtn, currentLocBtn;
 
     // variables for calculating and drawing a route
     private DirectionsRoute currentRoute;
@@ -88,7 +91,7 @@ public class MapBox extends AppCompatActivity implements
     private static final String LAYER_ID = "LAYER_ID";
 
     private SymbolManager symbolManager;
-    private Symbol symbol;
+    private Symbol symbol, symbol1, symbol2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,44 +108,53 @@ public class MapBox extends AppCompatActivity implements
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
-        List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
 
-        //To set the theme of mapbox Dark.
-        //mapboxMap.setStyle(Style.DARK);
-            // Custom styles can be formed on mapbox and implemented here
-        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/kalashshah11/ck5l9iwz83mqa1iqnj5twiamg")
-                // "mapbox://styles/mapbox/cjf4m44iw0uza2spb3q0a7s41"
-                // mapbox://styles/kalashshah11/ck5l9iwz83mqa1iqnj5twiamg
-                // Add the SymbolLayer icon image to the map style
-                // To change the image of coins
-                .withImage(ICON_ID, BitmapFactory.decodeResource(
-                        MapBox.this.getResources(), R.drawable.bitcoinsmall))
-                // Adding a GeoJson source for the SymbolLayer icons.
-                .withSource(new GeoJsonSource(SOURCE_ID,
-                        FeatureCollection.fromFeatures(symbolLayerIconFeatureList)))
-                // Adding the actual SymbolLayer to the map style. An offset is added that the bottom of the red
-                // marker icon gets fixed to the coordinate, rather than the middle of the icon being fixed to
-                // the coordinate point. This is offset is not always needed and is dependent on the image
-                // that you use for the SymbolLayer icon.
-                .withLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
-                        .withProperties(iconImage(ICON_ID),
-                                iconAllowOverlap(true),
-                                iconOffset(new Float[]{0f, -9f}))
-                ), new Style.OnStyleLoaded() {
+        //Adding images of coins
+        List<LatLng> locationList = new ArrayList<>();
+        locationList.add(new LatLng(28.362334, 75.585614));
+        locationList.add(new LatLng(28.362230, 75.585002));
+        locationList.add(new LatLng(28.361645, 75.585753));
+
+        for(int i=0; i<3; i++) {
+            MarkerOptions options = new MarkerOptions();
+            // to customize the marker
+            IconFactory iconFactory = IconFactory.getInstance(MapBox.this);
+            Icon icon = iconFactory.fromResource(R.drawable.bitcoinsmall);
+            options.icon(icon);
+            options.setPosition(locationList.get(i));  //location of each elemnt in array list
+            mapboxMap.addMarker(options);
+            mapboxMap.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(@NonNull Marker marker) {
+                    Intent intent = new Intent(MapBox.this, Vuforia.class);
+                    startActivity(intent);
+                    return true;
+                }
+            });
+        }
+
+
+
+
+        // Custom styles can be formed on mapbox and implemented here
+        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/naman016/ck5tn4s750nvn1iqf6ce0osbe"), new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
                 enableLocationComponent(style);
 
+                //GPS checker
                 LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                     Toast.makeText(MapBox.this, "GPS is Enabled in your devide", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     showGPSDisabledAlertToUser();
                 }
+
+
                 addDestinationIconSymbolLayer(style);
                 mapboxMap.addOnMapClickListener(MapBox.this);
 
+                //navigation search bar
                 navBtn = findViewById(R.id.nav_btn);
                 navBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -154,6 +166,8 @@ public class MapBox extends AppCompatActivity implements
                         NavigationLauncher.startNavigation(MapBox.this, options);
                     }
                 });
+
+                // current location button
                 currentLocBtn = findViewById(R.id.back_to_current_location);
                 currentLocBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -168,37 +182,10 @@ public class MapBox extends AppCompatActivity implements
                                 .newCameraPosition(position), 1500);
                     }
                 });
-                // Set up a SymbolManager instance
-                symbolManager = new SymbolManager(mapView, mapboxMap, style);
-                symbolManager.setIconAllowOverlap(false);
-                symbolManager.setTextAllowOverlap(false);
-                // Add symbol at specified lat/lon
-                symbol = symbolManager.create(new SymbolOptions()
-                        .withLatLng(new LatLng(28.362230, 75.585002))
-                        .withIconImage(ICON_ID)
-                        .withIconSize(0.5f)
-                        .withDraggable(false));
-                symbol = symbolManager.create(new SymbolOptions()
-                        .withLatLng(new LatLng(28.362334, 75.585614))
-                        .withIconImage(ICON_ID)
-                        .withIconSize(0.5f)
-                        .withDraggable(false));
-                symbol = symbolManager.create(new SymbolOptions()
-                        .withLatLng(new LatLng(28.361645, 75.585753))
-                        .withIconImage(ICON_ID)
-                        .withIconSize(0.5f)
-                        .withDraggable(false));
-                // Add click listener
-                symbolManager.addClickListener(new OnSymbolClickListener() {
-                    @Override
-                    public void onAnnotationClick(Symbol symbol) {
-                        Intent i = new Intent(MapBox.this,Vuforia.class);
-                        startActivity(i);
-                    }
-                });
             }
         });
     }
+
     //tO GIVE PERMISSION TO USE LOCATION
     @SuppressWarnings({"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
@@ -230,13 +217,16 @@ public class MapBox extends AppCompatActivity implements
             permissionsManager.requestLocationPermissions(this);
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
     @Override
     public void onExplanationNeeded(List<String> permissionsToExplain) {
     }
+
     @Override
     public void onPermissionResult(boolean granted) {
         if (granted) {
@@ -293,19 +283,12 @@ public class MapBox extends AppCompatActivity implements
         mapView.onDestroy();
     }
 
-    @SuppressWarnings( {"MissingPermission"})
+
+    @SuppressWarnings({"MissingPermission"})
     @Override
     public boolean onMapClick(@NonNull LatLng point) {
 
-        // Add click listener
-        symbolManager.addClickListener(new OnSymbolClickListener() {
-            @Override
-            public void onAnnotationClick(Symbol symbol) {
-                Intent i = new Intent(MapBox.this,Vuforia.class);
-                startActivity(i);
-            }
-        });
-
+       //Add marker to the clicked point
         Point destinationPoint = Point.fromLngLat(point.getLongitude(), point.getLatitude());
         assert locationComponent.getLastKnownLocation() != null;
         Point originPoint = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
@@ -315,6 +298,8 @@ public class MapBox extends AppCompatActivity implements
         if (source != null) {
             source.setGeoJson(Feature.fromGeometry(destinationPoint));
         }
+
+        //Forms the path to the destination
         getRoute(originPoint, destinationPoint);
         navBtn.setEnabled(true);
         navBtn.setBackgroundResource(R.color.mapboxBlue);
@@ -344,7 +329,7 @@ public class MapBox extends AppCompatActivity implements
                 .getRoute(new Callback<DirectionsResponse>() {
                     @Override
                     public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
-                         // You can get the generic HTTP info about the response
+                        // You can get the generic HTTP info about the response
                         Log.d(TAG, "Response code: " + response.code());
                         if (response.body() == null) {
                             Log.e(TAG, "No routes found, make sure you set the right user and access token.");
@@ -362,28 +347,32 @@ public class MapBox extends AppCompatActivity implements
                         }
                         navigationMapRoute.addRoute(currentRoute);
                     }
+
                     @Override
                     public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
                         Log.e(TAG, "Error: " + throwable.getMessage());
                     }
                 });
     }
-    private void showGPSDisabledAlertToUser(){
+
+
+    // gps dialog box
+    private void showGPSDisabledAlertToUser() {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MapBox.this);
         alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?")
                 .setCancelable(false)
                 .setPositiveButton("Goto Settings Page To Enable GPS",
-                        new DialogInterface.OnClickListener(){
-                            public void onClick(DialogInterface dialog, int id){
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
                                 Intent callGPSSettingIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                                 startActivity(callGPSSettingIntent);
                             }
                         });
-                alertDialogBuilder.setNegativeButton("Cancel",
-                new DialogInterface.OnClickListener(){
-                    public void onClick(DialogInterface dialog, int id){
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
-                        Intent intent = new Intent(MapBox.this,MainActivity.class);
+                        Intent intent = new Intent(MapBox.this, MainActivity.class);
                         startActivity(intent);
                     }
                 });
